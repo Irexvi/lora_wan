1b7f6a536275eccee71e8b40d7aeb1e0408707c0
Gabriel Rodríguez <gabriel.rodriguez199718@gmail.com>
2022-05-04 14:42:58 -0500

    Envío MQTT.

--- receive_upload.ino
+++ receive_upload.ino
@@ -54,15 +54,58 @@ int savedMonth;
 int savedDay;
 int savedTime;
 int hora_panama;
+uint32_t lastSentTime = 0UL;
+uint32_t lastSentTime1 = 0UL;
+uint32_t lastSentTime2 = 0UL;
+uint32_t lastSentTime8 = 0UL;
 // libraries needed for MQTT communication
 #include <PubSubClient.h>
 uint32_t counter = 0UL;
 
-IPAddress broker(199, 199, 199, 171);                      //define IP broker (Cambiar a la IP del servidor) Servidor: 192.168.18.10
+IPAddress broker(172, 20, 10, 127);                      //define IP broker (Cambiar a la IP del servidor)
 IPAddress namesServer(8, 8, 8, 8);                          //serverName:admin serverPassword:mxadm
 IPAddress netmask(255, 255, 255, 0);
-IPAddress gateway(199, 199, 6, 1);                         //define IP gateway 
+IPAddress gateway(172, 20, 10, 1);                         //define IP gateway 
 
+// Ethernet configuration values
+uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+uint8_t ip[] = { 172, 20, 10, 301 }; // CAMBIAR A IP DE COSTA DEL ESTE.
+
+//MQTT
+char mqttUsername[] = "telegraf";                           //Nombre de usuario MQTT
+char mqttPassword[] = "telegraf";                           //Contraseña MQTT
+short port_MQTT = 1883;                                     //define el puerto MQTT
+#define MQTT_ID ""
+// Initialize client MQTT
+void callback(char*topic, byte*payload, unsigned int length);
+EthernetClient client;
+PubSubClient mqtt(broker, 1883, callback, client);
+
+#define LUGAR "NELSON_COLLADO"
+#define SUCURSAL "CHITRE"
+#define TOPICO "sensores/nelson_collado"
+
+
+void callback(char*topic, byte*payload, unsigned int length)
+{
+  StaticJsonDocument<256> doc;
+
+  DeserializationError error = deserializeJson(doc, payload, length);
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+}
+
+void setup_MQTT()
+{
+
+  mqtt.setServer(broker, port_MQTT);
+  mqtt.setCallback(callback); //Definicion de callback
+
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600L);
@@ -85,8 +128,73 @@ void setup() {
   }
 
   Serial.println("Send started");
-}
 
+  // init Ethernet connection.
+  Ethernet.begin(mac, ip);
+  Serial.println(Ethernet.localIP());
+  
+  setup_MQTT();
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+String digitalClockDisplay() {
+  // digital clock display of the time
+
+  /*
+    Serial.print(hour());
+    printDigits(minute());
+    printDigits(second());
+    Serial.print(" ");
+    Serial.print(day());
+    Serial.print(" ");
+    Serial.print(month());
+    Serial.print(" ");
+    Serial.print(year());
+    Serial.println();
+  */
+
+  return (String(hour()) + printDigits(minute()) + printDigits(second()) + " " + String(day()) + "/" + String(month()) + "/" + String(year()));
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+String printDigits(int digits) {
+  // utility function for digital clock display: prints preceding colon and leading 0
+  //Serial.print(":");
+  if (digits < 10)
+    //Serial.print('0');
+    return ":0" + String(digits);
+  //Serial.print(digits);
+
+  return (":" + String(digits));
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void reconnect() {
+  if (mqtt.connect(MQTT_ID, mqttUsername, mqttPassword)) {
+    mqtt.subscribe("sensores/control"); //Topico de Control
+  } else {
+    // MQTT connect fail
+    Serial.println(digitalClockDisplay() + " - Error MQTT");
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void loop_MQTT() {
+
+  if (millis() - lastSentTime8 > 1000) {
+
+
+    if (!mqtt.connected()) {
+      reconnect();
+    } else {
+      mqtt.loop();
+    }
+
+    // avoid having delays in loop, we'll use the strategy from BlinkWithoutDelay
+    // see: File -> Examples -> 02.Digital -> BlinkWithoutDelay for more info
+
+    lastSentTime8 = millis();
+
+
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
   // Wait packets from Send example
@@ -96,8 +204,20 @@ void loop() {
 
     // Save the received packet payload into value
     if (LoRa.recv((uint8_t *) &value, &len)) {
-      Serial.print("Received value: ");
-      Serial.println(value);
+      //Serial.print("Received value: ");
+      //Serial.println(value);
+
+      char valtem[15];
+      String payload;
+      //Payload para los 5 registros del primer esclavo.
+      dtostrf(value, 2, 2, valtem); //convierto en String tank_h
+      payload = String(LUGAR)+String(",")+String("Sucursal=")+String(SUCURSAL)+String(",Sensor=NIVL");
+      //Serial.println(payload);
+      payload = payload + " Valor=" + valtem;
+      Serial.println(payload);
+      mqtt.publish(TOPICO, (char*) payload.c_str());
     }
   }
+
+  loop_MQTT(); // MQTT
 }
