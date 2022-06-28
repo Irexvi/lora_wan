aadbfcc7f81590fd2cf6b2c3c32825fcafe22e4a
Gabriel Rodríguez <gabriel.rodriguez199718@gmail.com>
2022-06-28 10:00:29 -0500

    Add files via upload

--- extraction_send.ino
+++ extraction_send.ino
@@ -14,13 +14,14 @@
    You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+//#include <avr/wdt.h>
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <ModbusTCPMaster.h>
 #include <ModbusTCPSlave.h>
 #include <LoRa.h>
-uint32_t nivel = 0UL;
+
 #include <EthernetUdp.h>
 #include <ArduinoJson.h>
 #include <math.h>
@@ -34,6 +35,11 @@ uint32_t nivel = 0UL;
 #include <PubSubClient.h>
 
 // Data constant for the setupRTCini() used only if NTP is not available
+int gatew = 1;
+int loraw = 0;
+int slave_id_1 = 19;
+int slave_id_2 = 20;
+
 
 const int YEAR = 2020;
 const int MONTH = 02;
@@ -44,7 +50,6 @@ const int SECOND = 00;
 char* valor;
 char* location;
 
-
 // Variable to save the Time for calculation
 int savedHour;
 int savedSecond;
@@ -54,55 +59,35 @@ int savedMonth;
 int savedDay;
 int savedTime;
 int hora_panama;
-
-
-
-float niv_min_oper = 0.37;
-float niv_min_stop = 0.12;
-
-
 uint32_t lastSentTime = 0UL;
 uint32_t lastSentTime1 = 0UL;
 uint32_t lastSentTime2 = 0UL;
-uint32_t lastSentTime8 = 0UL;
+uint32_t lastSentTime3 = 0UL;
 const uint32_t baudrate = 19200UL;
-const float tank_q = 6.0;  // cantidad de tanques
-const float tank_r = 119.0;  // radio de tanque
-const float tank_w = 47.0;  // radio de tanque
-const float tank_l = 235.0;  // radio de tanque
-
-const float tank_max_vol = 2.0;  // en m3
-const float max_lvl = 1.70;  // max columna de agua en cm
-const float min_lvl = 0.12;  // separacion de la boya del fondo en cm
-const float gal_multiply = 264.172;  // para convertir m3 --> gal
 
 int count = 0;
-#define NOMBRE "NC"
+#define NOMBRE "NAME"
 
 //// SETEO DE ESCLAVOS Y MAESTROS
 // Ethernet configuration values
 
-/*
-  Bahía Motors
-
-  Router 4G: 192.168.18.1
-
-  Servidor:  192.168.18.10
-  Puerto Grafana: 3000
-*/                     
 // Ethernet configuration values
 uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-uint8_t ip[] = { 172, 20, 10, 78 };
+uint8_t ip[] = { X, X, X, X }; // 192.168.1.1
 
-uint8_t slaveIp[] = { 172, 20, 10, 68 }; // Sensor de nivel
-uint16_t slavePort = 502; //502
+uint8_t slaveIp[] = { X, X, X, X }; // Sensor de nivel
+uint16_t slavePort = X; //Port
 
+uint8_t slave2Ip[] = { X, X, X, X }; // Sensor de caudal
+uint16_t slave2Port = X; //Port
 
 // Define the ModbusTCPMaster object
 ModbusTCPMaster master;
+ModbusTCPMaster master2;
 
 // Ethernet client object used to connect to the slave
 EthernetClient slave;
+EthernetClient slave2;
 
 EthernetClient client;
 
@@ -150,20 +135,9 @@ bool digitalInputs[numDigitalInputs];
 uint16_t analogOutputs[numAnalogOutputs];
 uint16_t analogInputs[numAnalogInputs + 23];
 
-/////// Combine función Piloto //////////
-float combine(unsigned int a, unsigned int b) {
-  unsigned int variable;
-  unsigned int signo;
-  unsigned int exponente;
-  unsigned int faccion;
-  float resultado;
-  variable = b * 65536 + a;
-  signo = variable >> 31;
-  exponente = (variable >> 23) & 255;
-  faccion = variable & 8388607;
-  resultado = ((-1) * exp(signo));
-  resultado = resultado * (2 * exp(exponente - 127) * (1 + (faccion / 8388608)));
-  return resultado;
+float combine_real(unsigned int a, unsigned int b){
+  unsigned long temp = (unsigned long) a << 16 | b;
+  return *(float*)&temp;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -191,7 +165,7 @@ void sendRequest(const char * address) {
     }
   }
 }
-////////////////////////////////////////////////////////////////////////////////
+
 //////////////////////// Setup RTC /////////////////////////////////////////////
 void setup_RTC()
 {
@@ -207,20 +181,6 @@ void setup_RTC()
 
 String digitalClockDisplay() {
   // digital clock display of the time
-
-  /*
-    Serial.print(hour());
-    printDigits(minute());
-    printDigits(second());
-    Serial.print(" ");
-    Serial.print(day());
-    Serial.print(" ");
-    Serial.print(month());
-    Serial.print(" ");
-    Serial.print(year());
-    Serial.println();
-  */
-
   return (String(hour()) + printDigits(minute()) + printDigits(second()) + " " + String(day()) + "/" + String(month()) + "/" + String(year()));
 }
 
@@ -295,9 +255,89 @@ void loop_processIMAGE() {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void loop_Modbus_TCP_M_G1() {
 
+void loop_LoRaWAN(){
+switch (loraw){
+
+case 0:
+     lastSentTime2=millis();
+     loraw=1; 
+     break; 
   
+case 1:
+      if (millis() - lastSentTime2 > 2000) {
+      //float caudal_f = (combine_real(analogInputs[numAnalogInputs+1],analogInputs[numAnalogInputs+2])); // Caudal
+      float caudal_f = analogInputs[numAnalogInputs+1];
+      float lvl_f = (analogInputs[numAnalogInputs]/100.0); // lvl
+      
+      char str_caud[6];
+      char str_nvl[6];
+      
+      dtostrf(caudal_f, 4, 2, str_caud);
+      dtostrf(nivel_f, 4, 2, str_nvl);
+      
+      uint8_t buffer_s[45];
+      
+      String payload;
+      
+      sprintf(buffer_s, "{\"CAUD\":%s,\"NVL\":%s}",str_caud,str_nvl);
+      
+      LoRa.send((uint8_t*) &buffer_s, sizeof(buffer_s));
+      Serial.println("Sending payload...");
+      LoRa.waitPacketSent();
+      Serial.println("Package sent.");
+      loraw=0;
+      break;
+  }
+  
+  } 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop_Modbus_TCP_M() {
+switch (gatew){
+case 1:
+  ////// CAUDAL INICIO ////////
+  if (!slave2.connected()) {
+        slave2.stop();
+        slave2.connect(slave2Ip, slave2Port);
+        Serial.println("Error de conexión.");
+      }
+
+      // Send a request every 1000ms if connected to slave
+      if (slave2.connected()) {
+        if (millis() - lastSentTime > 500) {
+
+          //if (!master2.readHoldingRegisters(slave2, slave_id_1, 0, 4)
+          if (!master2.readInputRegisters(slave2, slave_id_1, 1, 2)) {
+            // Failure treatment
+
+          }
+          lastSentTime = millis();
+        }
+        // Check available responses often
+        if (master2.isWaitingResponse()) {
+          ModbusResponse response = master2.available();
+          if (response) {
+            if (response.hasError()) {
+              // Response failure treatment. You can use response.getErrorCode()
+
+              // to get the error code.
+            } else {
+              analogInputs[numAnalogInputs+1]=response.getRegister(0); // getRegister(1)
+              analogInputs[numAnalogInputs+2]=response.getRegister(1); // getRegister(2)
+ 
+              gatew=2;
+              break;
+            }
+          }
+        }
+      }
+
+  ////// CAUDAL FINAL ////////
+
+  ////// NIVEL INICIO ///////
+case 2:
   if (!slave.connected()) {
         slave.stop();
         slave.connect(slaveIp, slavePort);
@@ -307,56 +347,34 @@ void loop_Modbus_TCP_M_G1() {
       // Send a request every 1000ms if connected to slave
       if (slave.connected()) {
         if (millis() - lastSentTime > 500) {
-          // Send a Read Input Registers request to the slave with address 3
-          // It requests for 6 registers starting at address 0
-          // IMPORTANT: all read and write functions start a Modbus transmission, but they are not
-          // blocking, so you can continue the program while the Modbus functions work. To check for
-          // available responses, call master.available() function often.
-          int slave_id = 1;
-          // !master.readInputRegisters(slave, slave_id, starting_address, quantity)
-          if (!master.readHoldingRegisters(slave, slave_id, 0, 1)) {
+
+          //if (!master.readHoldingRegisters(slave, slave_id_2, 1, 1)
+          if (!master.readInputRegisters(slave, slave_id_2, 1, 1)) {
             // Failure treatment
-
           }
-
           lastSentTime = millis();
         }
-
         // Check available responses often
         if (master.isWaitingResponse()) {
           ModbusResponse response = master.available();
           if (response) {
             if (response.hasError()) {
               // Response failure treatment. You can use response.getErrorCode()
-
               // to get the error code.
             } else {
-
-              
-
               analogInputs[numAnalogInputs] = response.getRegister(0); //Nivel en cm
-
-              nivel = analogInputs[numAnalogInputs];
-
-              ///////////////////////////////////////////////////////////////////////////////
-
-              //ENVÍO LoRa WAN AQUIII!!!!!
               
-              // Send counter as packet payload
-              LoRa.send((uint8_t*) &nivel, sizeof(nivel));
-              Serial.println("Sending nivel...");
-              // Wait packet to be sent
-              LoRa.waitPacketSent();
-              Serial.println("nivel sent!");
+              gatew=1;
+              break;
+              
             }
-            //comando1 = 1;
           }
         }
-        //break;
       }
+  
+  ///// NIVEL FINAL ////////
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -366,37 +384,28 @@ void slave_connect() {
     slave.stop();
     slave.connect(slaveIp, slavePort);
   }
-  Serial.println("Slave connected.");
+  Serial.println("Slave 1 connected.");
+
+  if (!slave2.connected()) {
+    slave2.stop();
+    slave2.connect(slave2Ip, slave2Port);
+  }
+  Serial.println("Slave 2 connected.");
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
 
   Serial.begin(9600UL);
-  while (!Serial) {
-    ;
-  }
-  Serial.println(F("Arrancando..."));
-  delay(2);
-
+  
   //LORA WAN
   if (!LoRa.begin()) {
     Serial.println("LoRa begin error: is LoRa module connected?");
     while (true);
   }
-
-  // Default LoRa values after begin:
-  // Frequency: 434.0MHz
-  // Modulation GFSK_Rb250Fd250
-  // TX power: +13dBm
-
-  // Set LoRa working frequency
-  if (!LoRa.setFrequency(869.0)) {
+  if (!LoRa.setFrequency(902.0)) {
     Serial.println("LoRa set frequency error");
     while (true);
   }
-  
-  Serial.println("Send started");
-
   //////////////////////////////////
   slave_connect();
 
@@ -407,20 +416,20 @@ void setup() {
   setup_RTC();
   setup_processIMAGE();
 
+  lastSentTime2 = millis();
+  lastSentTime3 = millis();
   // NOTE: it is not necessary to start the modbus master object
+  //wdt_enable(WDTO_4S);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-  if (Serial.available()) {
-    time_t t = processSyncMessage();
-    if (t != 0) {
 
-      RTC.set(t);   // set the RTC and the system time to the received value
-      setTime(t);
-    }
-  }
+//wdt_reset();
 
-  loop_Modbus_TCP_M_G1(); // Gateway 1
-  loop_processIMAGE();        
+loop_Modbus_TCP_M(); //Extracción TCP/IP Caudal
+loop_LoRaWAN(); //Envío LoRaWAN
+loop_processIMAGE();
+delay(1000);
+
 }
